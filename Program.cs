83ba2c19185abc654a26c8c147d6d35f8a0a1594
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-18 22:29:21 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diamond_Draw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Diamond_Draw
+
+Write a console that displays a diamond using stars. 
+
+
+ //         *
+ //        * *
+//        * * *
+ //        * *
+  //        *
