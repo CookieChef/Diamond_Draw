1037d3f525467b74f6495ce54c1e752f1bf6727d
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-29 22:54:27 -0400

    coding

--- Program.cs
+++ Program.cs
@@ -6,38 +6,15 @@ namespace Diamond_Draw
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many lines would you like your diamond to have?");
-            int n = int.Parse(Console.ReadLine());
-
-            //first half of the diamond
-
-            //number of times it will print a line
-            for (int i=1; i<=n; i++ ){
-                //adds the space in between *
-                for (int j=0; j<(n-i); j++)
-                    Console.Write(" ");
-                    //writes the * n amount of times
-                    for(int j=1; j<=i; j++)
-                    Console.Write("*");
-                   
-                    for (int k=1; k<i; k++)
-                    Console.Write("*");
-                    Console.WriteLine();                
+            var stars=5;
+        
+        for (var i=1; i<=stars; i++){
+            Console.Write("x");
+            for(var j=0; j<=5; j++){
+                Console.Write(" ");
             }
-            // second half of the diamond - goes in reverse.
-             
-            for (int i = n - 1; i >= 1; i--)
-            {
-                for (int j = 0; j < (n - i); j++)
-                    Console.Write(" ");
-                for (int j = 1; j <= i; j++)
-                    Console.Write("*");
-                for (int k = 1; k < i; k++)
-                    Console.Write("*");
-                Console.WriteLine();
-            }
- 
-            Console.WriteLine();
+        }
+        
         }
     }
 }
