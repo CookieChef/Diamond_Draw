4f2320b1fe77f8aa1103fd57366e467e743f360f
Sara Martinez <s_martinez_1990@outlook.com>
2020-09-30 22:46:47 -0400

    first half completed

--- Program.cs
+++ Program.cs
@@ -6,15 +6,36 @@ namespace Diamond_Draw
     {
         static void Main(string[] args)
         {
+        //-----------first half of diamond---------------
             var stars=5;
         
-        for (var i=1; i<=stars; i++){
-            Console.Write("x");
-            for(var j=0; j<=5; j++){
+        for (var i=0; i<=stars; i++){
+             for(var lineNumber = i; lineNumber<=stars-1; lineNumber++){
                 Console.Write(" ");
             }
+            for(var j=i; j>=1; j--){
+                //Console.Write(i);
+                Console.Write("*");
+                Console.Write(" ");  
+                          
+            }
+           
+            Console.WriteLine();
+            
         }
+
+        //-----------first half of diamond---------------\
+        
         
         }
     }
 }
+
+//    *     4 spaces
+//   * *    3 spaces
+//  * * *   2 space
+// * * * *  1 spaces
+//* * * * * 0 spaces
+
+//when lineNumber = stars, spaces = 0
+//when lineNumber = 1, spaces = stars-1;
